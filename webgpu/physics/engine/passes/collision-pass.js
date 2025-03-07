e64fa05415e6e3657ab066fa7d04dc9ab1601e75
codehipster <oostdam@gmail.com>
2025-03-07 10:15:11 +0100

    wip collision

--- webgpu/physics/engine/passes/collision-pass.js
+++ webgpu/physics/engine/passes/collision-pass.js
@@ -1,10 +1,28 @@
 const shader = /*wgsl*/`
   const MAX_PARTICLES_PER_CELL: u32 = 30; // 1000 * 1000 * 30 is close to 128mb max storage space. Max size is 1000
+  const OUT_OF_BOUNDS: u32 = 4294967295;
+  const offsets = array<vec2<i32>, 9>(
+    vec2(-1, -1), vec2(0, -1), vec2(1, -1),  // Top neighbors
+    vec2(-1, 0), vec2(0, 0), vec2(1, 0),     // Left and Right
+    vec2(-1, 1), vec2(0, 1), vec2(1, 1)      // Bottom neighbors
+  );
+
+  // Storage Buffers
+  @group(0) @binding(0) var<storage, read_write> particles: array<Particle>; 
+  @group(0) @binding(1) var<uniform> globals: GlobalVars;
+  @group(0) @binding(2) var<storage, read_write> grid: array<u32>; // Stores particle indices
+  @group(0) @binding(3) var<storage, read_write> grid_count: array<atomic<u32>>; // Particle count per cell
+  @group(0) @binding(4) var<storage, read_write> particle_displacement: array<atomic<i32>>; // Particle displacement 2 ints for each particle. x & y
 
   struct Collision {
     hit: bool,
-    vector: vec2i,
-    magSq: i32,
+    diff: vec2i,
+    mag_sq: i32,
+  }
+
+  struct Neighbours {
+    count: u32,
+    neighbour: array<u32, 270>
   }
 
   struct GlobalVars {
@@ -22,29 +40,54 @@ const shader = /*wgsl*/`
     prev_position: vec2<i32>,
   };
 
-  // Storage Buffers
-  @group(0) @binding(0) var<storage, read_write> particles: array<Particle>; 
-  @group(0) @binding(1) var<uniform> globals: GlobalVars;
-  @group(0) @binding(2) var<storage, read_write> grid: array<u32>; // Stores particle indices
-  @group(0) @binding(3) var<storage, read_write> grid_count: array<atomic<u32>>; // Particle count per cell
-  @group(0) @binding(4) var<storage, read_write> particle_displacement: array<atomic<i32>>; // Particle displacement 2 ints for each particle. x & y
+  // Get byte index of grid[0 -> SIZE] in the array
+  // The array is concatenated rows
+  // with each cell taking (x + y) * 30 bytes.
+  // pos is expected to be a position on the grid [0,SIZE]
+  fn arrayIndexOffset(array_index: u32) -> u32 {
+    return array_index * MAX_PARTICLES_PER_CELL; //
+  }
 
-  // Hash function: Maps a position to a grid index
-  fn gridIndex(pos: vec2<i32>) -> u32 {
+  fn arrayIndex(grid_pos: vec2<i32>) -> u32 {
+    return u32(grid_pos.x + (grid_pos.y * globals.size));
+  }
+
+  fn positionOnGrid(pos: vec2<i32>) -> vec2<i32> {
     // To move from signed ints to unsigned ints, 
     let align = globals.size / 2;
     // the width of the grid.
-
     let x = (pos.x / globals.physics_scale) + align;
     let y = (pos.y / globals.physics_scale) + align;
-
-    return u32(y * globals.size + x);
+    return vec2i(x, y);
   }
 
-  fn getNeighbours(grid_index: u32) -> array<u32> {
-    // get particles from all 9 cells
-    // TODO implement
-    return array<u32>(1, 2, 3);
+  // return type is max value that we could have
+  // position = the position on the grid. [0,SIZE]
+  // TODO: optimize with storage buffer?
+  fn getNeighbours(position: vec2i) -> Neighbours {
+
+    var neighbour = array<u32,270>();
+    var count = 0;
+    for (var i: u32 = 0; i < 9; i = i + 1) {
+      let grid_index = position + offsets[i];
+
+      // Ensure the neighbor is within grid bounds
+      if (grid_index.x >= 0 && grid_index.x < globals.size && grid_index.y >= 0 && grid_index.y <globals.size) {
+        let count_index = arrayIndex(grid_index);  // index of count array, which has just 1 u32 for each cell.
+        let grid_index = arrayIndexOffset(count_index); // index in the grid array which has index * 30 per cell.
+        let n_count = grid_count[count_index];
+          
+        // fetch count of neighbours
+        for (var c: u32 = 0; c < n_count; c = c + 1) {
+          let n_index = grid[grid_index + c];
+          neighbour[n_count] = n_index;
+          count = count + 1;
+        }
+      }
+    }
+
+    // Count will be the nr of neighbours (not the last index)
+    return Neighbours(count,neighbour);
   }
 
   fn distanceSquared(v : vec2i) -> i32 {
@@ -71,18 +114,16 @@ const shader = /*wgsl*/`
 
       let particle_index = id.x;
       let p = particles[particle_index];
-      let grid_index = gridIndex(p.position);
+      let neighbours = getNeighbours(p.position);
 
-      let neighbours = getNeighbours(grid_index);
-
-      for (var i: u32 = 0; i < arrayLength(neighbours); i = i + 1) {
-        let n_index = neighbours[i]; // get neighbour index
+      for (var i: u32 = 0; i < neighbours.count ; i = i + 1) {
+        let n_index = neighbours.neighbour[i]; // get neighbour index
         let n = particles[n_index];
         
         var collision = collides(p, n);
         if(!collision.hit) {continue;}
 
-        var displacement = bounce(diff, magnitude_squared);
+        var displacement = bounce(collision.diff, collision.mag_sq);
 
         // set displacement values in buffer
         // Use atomicAdd to ensure safe writing
@@ -186,6 +227,7 @@ export default class CollisionPass {
 
   _bindGroup(device, globalsBuffer, particleBuffer, gridBuffer, gridCountBuffer, displacementBuffer, pipeline) {
     return device.createBindGroup({
+      label: 'collision-bindgroup',
       layout: pipeline.getBindGroupLayout(0),
       entries: [
         { binding: 0, resource: { buffer: particleBuffer } },
