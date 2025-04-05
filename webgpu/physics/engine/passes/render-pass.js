5dca7a5334a5c77692ea226d533dbb30284259e0
Thijs Oostdam <thijs@oostd.am>
2025-04-05 08:48:42 +0200

    WIP colors based on velocity

--- webgpu/physics/engine/passes/render-pass.js
+++ webgpu/physics/engine/passes/render-pass.js
@@ -1,3 +1,55 @@
+const shader = /*wgsl*/`
+  struct GlobalVars {
+    gravity: vec2i, // (x,y) acceleration
+    min: i32,
+    max: i32,
+    physics_scale: i32, // for scaling down to grid size
+    render_scale: i32, // for scaling down to clip_space
+    sps_2: i32, // steps per second squared
+    size: i32, // size of the simulation
+  };
+  
+  struct Particle {
+    position: vec2<i32>,
+    prev_position: vec2<i32>,
+  };
+
+  @group(0) @binding(0) var<storage, read> particles: array<Particle>;
+  @group(0) @binding(1) var<uniform> globals: GlobalVars;
+  @group(0) @binding(2) var<storage, read> colors: array<vec4f>;
+
+  struct VertexOutput {
+      @builtin(position) position: vec4<f32>,
+      @location(0) color: vec4<f32>,
+  };
+
+  @vertex
+  fn vs_main(@builtin(vertex_index) index: u32) -> VertexOutput {
+      var out: VertexOutput;
+      let particle = particles[index];
+      let f_velocity = vec2f(particle.position - particle.prev_position);
+      let f_dist_sq = dot(f_velocity, f_velocity);
+      let f_gravity_step = vec2f(globals.gravity / globals.sps_2);
+      let f_gravity_sq = dot(f_gravity_step, f_gravity_step)*20;
+      // fully white when velocity > gravity
+      // fully blue when velocity = 0
+      let color_scale = min(f_dist_sq / f_gravity_sq, 1);
+      let white = vec4f(1.0, 1.0, 1.0, 1.0);
+      let blue = vec4f(0.0, 0.0, 1.0, 1.0);
+      let color = colors[index];
+
+      // Scale to clip space [-1,1]
+      let f_pos = vec2f(f32(particle.position.x), f32(particle.position.y)) / f32(globals.render_scale); //possibly losing some precision here, but that should not affect where the particle ends up on screen too much.
+      out.position = vec4<f32>(f_pos, 0.0, 1.0); // Normalize to clip space
+      out.color = mix(blue, white, color_scale);
+      return out;
+  }
+
+  @fragment
+  fn fs_main(in: VertexOutput) -> @location(0) vec4<f32> {
+      return in.color;
+  }`
+
 export default class RenderPass {
 
   constructor(device, textureFormat, globalsBuffer, particleBuffer, colorBuffer, particleCount) {
@@ -57,45 +109,3 @@ export default class RenderPass {
     });
   }
 }
-
-const shader = /*wgsl*/`
-  struct GlobalVars {
-    gravity: vec2i, // (x,y) acceleration
-    min: i32,
-    max: i32,
-    physics_scale: i32, // for scaling down to grid size
-    render_scale: i32, // for scaling down to clip_space
-    sps_2: i32, // steps per second squared
-    size: i32, // size of the simulation
-  };
-  
-  struct Particle {
-    position: vec2<i32>,
-    prev_position: vec2<i32>,
-  };
-
-  @group(0) @binding(0) var<storage, read> particles: array<Particle>;
-  @group(0) @binding(1) var<uniform> globals: GlobalVars;
-  @group(0) @binding(2) var<storage, read> colors: array<vec4f>;
-
-  struct VertexOutput {
-      @builtin(position) position: vec4<f32>,
-      @location(0) color: vec4<f32>,
-  };
-
-  @vertex
-  fn vs_main(@builtin(vertex_index) index: u32) -> VertexOutput {
-      var out: VertexOutput;
-      let pos = particles[index].position; 
-      // Scale to clip space [-1,1]
-      let f_pos = vec2f(f32(pos.x), f32(pos.y)) / f32(globals.render_scale); //possibly losing some precision here, but that should not affect where the particle ends up on screen too much.
-      out.position = vec4<f32>(f_pos, 0.0, 1.0); // Normalize to clip space
-      out.color = colors[index];
-      return out;
-  }
-
-  @fragment
-  fn fs_main(in: VertexOutput) -> @location(0) vec4<f32> {
-      return in.color;
-  }
-`
